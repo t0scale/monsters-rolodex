5b53e2fe54c377309850b265052c0d3e26ab4645
t0scale <carlos.mendez.roca@gmail.com>
2020-04-18 00:16:11 -0700

    Updates

---
+++ precache-manifest.e15e32733c08a4e230ce59b675d98cce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6542d7bf06c67e8ebad96de2422376a4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e2ce0760c8196267dcd2",
+    "url": "/monsters-rolodex/static/css/main.cf812457.chunk.css"
+  },
+  {
+    "revision": "67342f4b90102ddf1b87",
+    "url": "/monsters-rolodex/static/js/2.2cba85e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.2cba85e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2ce0760c8196267dcd2",
+    "url": "/monsters-rolodex/static/js/main.88a58de2.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
